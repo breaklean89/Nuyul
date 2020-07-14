3ce76307220189c9f6b0b32b4aae3de7c5ac3cd6
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2020-07-14 08:00:40 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+$link = "http://ld-bitvpn-webserver-481332435.us-west-1.elb.amazonaws.com:8080/bitproxy/proxy/gwebsvr/infoBus?deviceId=And.6885958b51fd71c6787f856561fb601c.bitvpn&userId=426613198240509&token=e39148c68dec3350dbebd33b3a95b264&TrackCode=9916483082194950&appId=bitvpn&storeID=2&countryCode=62&clientVersion=1.2.9&timestamp=1594684666695&siteId=6";
+
