8c0c46606944d118cb258d5facdef88bd9b507f7
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2020-07-14 15:06:36 +0800

    Delete config.php

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$link = "http://ld-bitvpn-webserver-481332435.us-west-1.elb.amazonaws.com:8080/bitproxy/proxy/bitproxy/ip/get?deviceId=And.6885958b51fd71c6787f856561fb601c.bitvpn&userId=426613198240509&token=fc72168a4e16e850b97a5fafe3a1ea64&TrackCode=9916482503327750&appVersion=1.2.9&dingtoneId=1767545&isBasic=1&osType=2&srcCountry=id&version=3&vpnType=8&zone=us&sign=F3F787848B0AE309EFF9449BCF78CB88&timestamp=1594696314237&siteId=6 ";
-
